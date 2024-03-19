8008491a673fbf8e29a76f8499e01b846985a658
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-03-19 20:01:20 +0100

    Update 02_boolean_and_conditionals.py

--- modern_p3_bootcamp/02_boolean_and_conditionals.py
+++ modern_p3_bootcamp/02_boolean_and_conditionals.py
@@ -107,7 +107,6 @@
 # y = randint(-100, 100)
 # while y == 0:
 #     y = randint(-100, 100)
-
 # if x > 0 and y > 0:
 #     print("both positive")
 # elif x < 0 and y < 0:
@@ -119,14 +118,11 @@
 
 # # Coding Excercise 12: Calling in Sick
 # from random import choice, randint
-
 # actually_sick = choice([True, False])
 # kinda_sick = choice([True, False])
 # hate_your_job = choice([True, False])
 # sick_days = randint(0, 10)
-
 # calling_in_sick = None  # set this to True or False with Boolean Logic and Conditionals!
-
 # if actually_sick and sick_days != 0:
 #     calling_in_sick = True
 # elif kinda_sick and hate_your_job and sick_days != 0:
