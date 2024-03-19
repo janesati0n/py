aab0d4a94e153f2492c74c9c569e55b2736edac1
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-03-19 15:48:46 +0100

    Update 02_boolean_and_conditionals.py

--- modern_p3_bootcamp/02_boolean_and_conditionals.py
+++ modern_p3_bootcamp/02_boolean_and_conditionals.py
@@ -29,4 +29,16 @@
 # else:
 #     print("even")
 
-# Multiple Elifs
+# # Multiple Elifs
+# color = input("What's your favorite color?")
+# if color == 'purple':
+#   print("Excellent choice!")
+# elif color == 'teal':
+#   print("Not bad!")
+# elif color == 'seafoam':
+#   print("Mediocre")
+# elif color == 'pure darkness':
+#   print("I like how you think")
+# else:
+#   print("YOU MONSTER!")
+
