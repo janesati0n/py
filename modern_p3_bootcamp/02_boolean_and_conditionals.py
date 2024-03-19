033671519269c4c8500b9dfe316d61826756f7c3
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-03-19 15:55:01 +0100

    Update 02_boolean_and_conditionals.py

--- modern_p3_bootcamp/02_boolean_and_conditionals.py
+++ modern_p3_bootcamp/02_boolean_and_conditionals.py
@@ -42,3 +42,12 @@
 # else:
 #   print("YOU MONSTER!")
 
+# # A Word on Truthiness
+# print("Enter your favorite animal")
+# animal = input()
+# if animal:
+#   print(animal + " is my favorite too!")
+# else:
+#   print("You didn't say anything!")
+
+# The Joy of Comparison Operators
