f83b06f85be46b574f4a6a1832ea3001ead8a5df
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-03-20 11:45:49 +0100

    Update 03_rockpaperscissors.py

--- modern_p3_bootcamp/03_rockpaperscissors.py
+++ modern_p3_bootcamp/03_rockpaperscissors.py
@@ -1,1 +1,17 @@
-
+# # Rock, Paper, Scissors
+# # Basic Version
+# print('...rock...')
+# print('...paper...')
+# print('...scissors...')
+# player1 = input("enter Player 1's choice: ")
+# player2 = input("enter Player 2's choice: ")
+# if player1 and player2:
+#   print('SHOOT!')
+#   if player1 == 'rock' and player2 == 'scissors' or player1 == 'scissors' and player2 == 'paper' or player1 == 'paper' and player2 == 'rock':
+#     print('player1 wins')
+#   elif player1 == 'rock' and player2 == 'rock' or player1 == 'scissors' and player2 == 'scissors' or player1 == 'paper' and player2 == 'paper':
+#     print('draw')
+#   else:
+#     print('player2 wins')
+# else:
+#   print('wrong input!')
