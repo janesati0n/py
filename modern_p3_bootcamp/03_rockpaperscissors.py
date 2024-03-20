83c5b2462aa79b4f35e0ddd7f93465c94aac9dc0
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-03-20 14:15:22 +0100

    Update 03_rockpaperscissors.py

--- modern_p3_bootcamp/03_rockpaperscissors.py
+++ modern_p3_bootcamp/03_rockpaperscissors.py
@@ -1,5 +1,5 @@
 # # Rock, Paper, Scissors
-# # My First Attempt
+# # Basic Version
 # print('...rock...')
 # print('...paper...')
 # print('...scissors...')
@@ -16,7 +16,7 @@
 # else:
 #   print('wrong input!')
 
-# # Basic Version (from video with refactoring)
+# # Refactoring
 # print('...rock...')
 # print('...paper...')
 # print('...scissors...')
@@ -43,4 +43,36 @@
 # else:
 #   print('something went wrong')
 
-# Playing against computer
+# # Playing against computer
+# from random import randint
+# print('...rock...')
+# print('...paper...')
+# print('...scissors...')
+# player = input("enter Player's choice: ")
+# rand_num = randint(0,2)
+# if rand_num == 0:
+#   computer = 'rock'
+# elif rand_num == 1:
+#   computer = 'paper'
+# else:
+#   computer = 'scissors'
+# print(f'The computer plays {computer}')
+# if player == computer:
+#   print('draw')
+# elif player == 'rock':
+#   if computer == 'scissors':
+#     print('Player wins')
+#   elif computer == 'paper':
+#     print('Computer wins')
+# elif player == 'scissors':
+#   if computer == 'paper':
+#     print('Player wins')
+#   elif computer == 'rock':
+#     print('Computer wins')
+# elif player == 'paper':
+#   if computer == 'rock':
+#     print('Player wins')
+#   elif computer == 'scissors':
+#     print('Computer wins')
+# else:
+#   print('Something went wrong')
