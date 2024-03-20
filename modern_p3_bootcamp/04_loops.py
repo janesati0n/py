e706c591c3c557c6d711b2a4c2fcacf25d645395
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-03-20 15:17:43 +0100

    Update 04_loops.py

--- modern_p3_bootcamp/04_loops.py
+++ modern_p3_bootcamp/04_loops.py
@@ -1,1 +1,13 @@
+# # For Loops
+# for number in range(1, 8):
+#   print(number)
+
+# for char in 'coffee':
+#   print(char)
+
+# Ranges
+# range(7)      # 0,1,2,3,4,5,6
+# range(1,8)    # 1,2,3,4,5,6,7
+# range(1,10,2) # 0,2,4,6,8,10
+# range(7,0,-1) # 7,6,5,4,3,2,1
 
