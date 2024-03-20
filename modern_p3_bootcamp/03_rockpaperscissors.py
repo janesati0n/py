e350c79c541e61da4ffeb3abe37f7f60a00af515
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-03-20 12:06:14 +0100

    Update 03_rockpaperscissors.py

--- modern_p3_bootcamp/03_rockpaperscissors.py
+++ modern_p3_bootcamp/03_rockpaperscissors.py
@@ -9,9 +9,35 @@
 #   print('SHOOT!')
 #   if player1 == 'rock' and player2 == 'scissors' or player1 == 'scissors' and player2 == 'paper' or player1 == 'paper' and player2 == 'rock':
 #     print('player1 wins')
-#   elif player1 == 'rock' and player2 == 'rock' or player1 == 'scissors' and player2 == 'scissors' or player1 == 'paper' and player2 == 'paper':
+#   elif player1 == player2:
 #     print('draw')
 #   else:
 #     print('player2 wins')
 # else:
 #   print('wrong input!')
+
+# # Refactoring
+# print('...rock...')
+# print('...paper...')
+# print('...scissors...')
+# player1 = input("enter Player 1's choice: ")
+# player2 = input("enter Player 2's choice: ")
+# if player1 == player2:
+#   print('draw')
+# elif player1 == 'rock':
+#   if player2 == 'scissors':
+#     print('player1 wins')
+#   elif player2 == 'paper':
+#     print('player2 wins')
+# elif player1 == 'scissors':
+#   if player2 == 'paper':
+#     print('player1 wins')
+#   elif player2 == 'rock':
+#     print('player2 wins')
+# elif player1 == 'paper':
+#   if player2 == 'rock':
+#     print('player1 wins')
+#   elif player2 == 'scissors':
+#     print('player2 wins')
+# else:
+#   print('something went wrong')
