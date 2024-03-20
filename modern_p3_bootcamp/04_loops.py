a41d38d82f4e7a8a4a5e101db1aeeb2bee2294b4
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-03-20 15:29:34 +0100

    Update 04_loops.py

--- modern_p3_bootcamp/04_loops.py
+++ modern_p3_bootcamp/04_loops.py
@@ -11,3 +11,14 @@
 # range(1,10,2) # 0,2,4,6,8,10
 # range(7,0,-1) # 7,6,5,4,3,2,1
 
+# # Coding Excercise 13: For Loop and Range
+# x = 0
+# for i in range(11,21,2):
+#   x += i
+#   print(i)
+
+# # Excercise: Screaming Repeating
+# times = input('How many times do I have to tell you? ')
+# times = int(times)
+# for i in range(times):
+#   print('CLEAN UP YOUR ROOM!')
