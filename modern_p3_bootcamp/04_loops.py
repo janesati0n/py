5143479fa2fa7957429fc454f9bdf6d7a70b1442
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-03-21 16:21:24 +0100

    Update 04_loops.py

--- modern_p3_bootcamp/04_loops.py
+++ modern_p3_bootcamp/04_loops.py
@@ -67,3 +67,16 @@
 # while reply != 'stop copying me':
 #   reply = input(f"{reply} ")
 # print('UGH FINE YOU WIN')
+
+# # The BREAK Keyword
+# while True:
+#   command = input("Type 'exit' to exit: ")
+#   if (command == "exit"):
+#     break
+
+# times = int(input('How many times do I have to tell you? '))
+# for time in range(times):
+#   print("CLEAN UP YOUR ROOM!")
+#   if time >= 3:
+#     print('do you even listen anymore?')
+#     break
