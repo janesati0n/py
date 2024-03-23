ef18402569c558a09985cd0b048f0edae9a3d0a8
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-03-23 23:28:43 +0100

    Update 05_guessing_game.py

--- modern_p3_bootcamp/05_guessing_game.py
+++ modern_p3_bootcamp/05_guessing_game.py
@@ -1,6 +1,6 @@
 # # First Attempt
 # from random import randint
-# number = randint(1,11)
+# number = randint(1,10)
 # guess = 0
 # play_again = 'y'
 # while play_again != 'n':
@@ -15,4 +15,23 @@
 #   print("You guessed it! You won!")
 #   play_again = input("Do you want to keep playing? (y/n) ")
 
-
+# # Solution
+# from random import randint
+# random_number = randint(1,10)
+# guess = None
+# while True:
+#   guess = input("Guess a number between 1 and 10: ")
+#   guess = int(guess)
+#   if guess < random_number:
+#     print("Too low, try again!")
+#   elif guess > random_number:
+#     print("Too high, try again!")
+#   else:
+#     print("You guessed it! You won!")
+#     play_again = input("Do you want to keep playing? (y/n) ")
+#     if play_again == 'y':
+#       random_number = randint(1,10)
+#       guess = None
+#     else:
+#       print('Thanks for playing. Bye!')
+#       break
