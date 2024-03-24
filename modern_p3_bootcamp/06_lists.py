0d807051a6f423d2ce3af079fc659be668aaa492
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-03-24 22:28:33 +0100

    Update 06_lists.py

--- modern_p3_bootcamp/06_lists.py
+++ modern_p3_bootcamp/06_lists.py
@@ -1,1 +1,14 @@
+# # Creating lists
+# tasks = ["Install Python", "Learn Python", "Take a break"]
+# len(tasks) # 3 (length of list)
+# tasks = list(range(1,4))
+# tasks
+# [1,2,3]
 
+# # Shopping List (first attempt)
+# shopping_list = ['applesauce', 'house plant', 'printer paper', 'protein powder', 'zebra print rug (faux)']
+# arana = None
+# while arana != 'q':
+#   arana = input("Add something to the cart? Type q to quit: ")
+#   shopping_list += arana
+# print(f"YOUR FINAL GROCERY LIST\n{arana}")
