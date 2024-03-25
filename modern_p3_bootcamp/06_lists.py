b71035e96bcb5f186189622fc4e389f2d95ba480
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-03-25 11:04:54 +0100

    Update 06_lists.py

--- modern_p3_bootcamp/06_lists.py
+++ modern_p3_bootcamp/06_lists.py
@@ -1,14 +1,26 @@
-# # Creating lists
-# tasks = ["Install Python", "Learn Python", "Take a break"]
-# len(tasks) # 3 (length of list)
-# tasks = list(range(1,4))
-# tasks
-# [1,2,3]
-
 # # Shopping List (first attempt)
 # shopping_list = ['applesauce', 'house plant', 'printer paper', 'protein powder', 'zebra print rug (faux)']
 # arana = None
 # while arana != 'q':
 #   arana = input("Add something to the cart? Type q to quit: ")
 #   shopping_list += arana
-# print(f"YOUR FINAL GROCERY LIST\n{arana}")
+# print("YOUR FINAL GROCERY LIST")
+# for i in shopping_list:
+#   print(i)
+
+# # Iterating Over Lists
+# colors = ['purple', 'teal', 'magenta']
+# for color in colors:
+#   print(color)
+
+# numbers = [1, 2, 3, 4]
+# i = 0
+# while i < len(numbers):
+#   print(numbers[i])
+#   i += 1
+
+# # List methods: Append, Insert and Extend
+# data = [1, 2, 3]
+# data.append(4) # adds value to the end [1, 2, 3, 4]
+# data.extend([5,6]) # adds multiple values to the end [1, 2, 3, 4, 5, 6]
+# data.insert(2, 'Hi!') # [1, 2, 'Hi!', 3, 4, 5 ,6]
