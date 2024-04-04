c418d02130d49ecea7a3895f8b5cc5192475c98b
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-04-04 19:05:54 +0200

    Update 04_loops.py

--- modern_p3_bootcamp/04_loops.py
+++ modern_p3_bootcamp/04_loops.py
@@ -1,91 +1,39 @@
-# # For Loops
-# for number in range(1, 8):
-#   print(number)
+# # Looping
+# numbers = [1, 2, 3, 4, 5]
+# doubled_numbers = []
+# for num in numbers:
+#   doubled_number = num * 2
+#   doubled_numbers.append(doubled_number)
+# print(doubled_numbers) # [2, 4, 6, 8, 10]
 
-# for char in 'coffee':
-#   print(char)
+# # List Comprehension
+# numbers = [1, 2, 3, 4, 5]
+# doubled_numbers = [num * 2 for num in numbers]
+# print(doubled_numbers) # [2, 4, 6, 8, 10]
 
-# # Ranges
-# range(7)      # 0,1,2,3,4,5,6
-# range(1,8)    # 1,2,3,4,5,6,7
-# range(1,10,2) # 0,2,4,6,8,10
-# range(7,0,-1) # 7,6,5,4,3,2,1
+# # Use with conditional logic
+# numbers = [1, 2, 3, 4, 5, 6]
+# evens = [num for num in numbers if num % 2 == 0]
+# odds = [num for num in numbers if num % 2 != 0]
+# print(f'odds: {odds}, evens: {evens}')
+# [num*2 if num % 2 == 0 else num/2 for num in numbers]
 
-# # Coding Excercise 13: For Loop and Range
-# x = 0
-# for i in range(11,21,2):
-#   x += i
-#   print(i)
+# # Coding Excercise 20
+# Create list of first letters for each name in given list
+# answer = [person[0] for person in ["Elie", "Tim", "Matt"]]
+# Create list of all even values in given list
+# answer2 = [val for val in [1,2,3,4,5,6] if val % 2 == 0]
 
-# # Excercise: Screaming Repeating
-# times = input('How many times do I have to tell you? ')
-# times = int(times)
-# for i in range(times):
-#   print('CLEAN UP YOUR ROOM!')
+# # Coding Excercise 21
+# Create list that is the intersection of two
+# answer = [num for num in [1,2,3,4] if num in [3, 4, 5, 6]]
+# Create list with each word reversed and in lower case
+# answer2 = [name[::-1].lower() for name in ['Elie', 'Tim', 'Matt']]
 
-# # Excercise: Unlucky numbers
-# for num in range(1,21):
-#   if num == 4 or num == 13:
-#      state = 'unlucky'
-#   elif num % 2 == 0:
-#     state = 'even'
-#   else:
-#     state = 'odd'
-#   print(f'{num} is {state}')
+# # Coding Excercise 22
+# Create list with all numbers divisible by 12 between 1 and 100
+# answer =  [num for num in range(1,101) if num % 12 == 0]
 
-# # WHILE LOOPS
-# msg = input("Ake je carovne slovicko? ")
-# while msg != 'prosim':
-#   print('NESPRAVNE')
-#   msg = input("Ake je carovne slovicko? ")
-# print('No vidis ze vies')
-
-# num = 0
-# while num <= 10:
-#   num += 1
-#   print(num)
-
-# # Excercise: Emoji Art
-# for num in range(1,11):
-#   print("*" * num)
-
-# times = 1
-# while times <= 10:
-#   print("*" * times)
-#   times += 1
-
-# for num in range(1,11):
-#   count = 1
-#   stars = ''
-#   while count <= num:
-#     stars += '*'
-#     count += 1
-#     print(stars)
-
-# # Excercise: Stop Copying Me
-# reply = input("Hey, how's it going? ")
-# while reply != 'stop copying me':
-#   reply = input(f"{reply} ")
-# print('UGH FINE YOU WIN')
-
-# # The BREAK Keyword
-# while True:
-#   command = input("Type 'exit' to exit: ")
-#   if (command == "exit"):
-#     break
-
-# times = int(input('How many times do I have to tell you? '))
-# for time in range(times):
-#   print("CLEAN UP YOUR ROOM!")
-#   if time >= 3:
-#     print('do you even listen anymore?')
-#     break
-
-# # Coding Excercise 14: While Loop
-# from random import randint
-# number = 0
-# i = 0
-# while number != 5:
-#     i += 1
-#     number = randint(1, 10)
-#     print(f"Your number is {number} and you repeated it {i} times")
+# # Coding Excercise 23
+# Create list containing letters from amazing but not the vowels
+# answer = [val for val in 'amazing' if val not in 'aeiou']
