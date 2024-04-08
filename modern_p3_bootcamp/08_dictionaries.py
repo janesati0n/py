33cbabe7c5104e397043cf158947d4a190e02ada
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-04-08 13:02:54 +0200

    Update 08_dictionaries.py

--- modern_p3_bootcamp/08_dictionaries.py
+++ modern_p3_bootcamp/08_dictionaries.py
@@ -11,7 +11,7 @@
 # cat = dict(name ='kitty')
 # cat = dict(name ='kitty', age=0.5)
 
-# # Coding Excercise 26
+# # Coding Excercise 26: Dictionary Creation Excercise
 # user_info = {
 #     'username': 'nbusr',
 #     'password': 'nbusr123',
@@ -22,7 +22,7 @@
 # instructor['name'] # 'Colt'
 # instructor['thing'] # KeyError
 
-# # Coding Excercise 27
+# # Coding Excercise 27: Access Data in Dictionary Excercise
 # artist = {
 #     "first": "Neil",
 #     "last": "Young",
@@ -31,3 +31,21 @@
 # full_name = artist['first'] + ' ' + artist['last'] # Solution 1: Concat method
 # full_name = f"{artist['first']} {artist['last']}"  # Solution 2: F-string method
 # full_name = "{} {}".format(artist["first"], artist["last"]) # Solution 3: Format method
+
+# # Iterating Dictionaries
+# for value in instructor.values():
+#   print(value)
+
+# for key in instructor.keys():
+#   print(key)
+
+# for key, value in instructor.items(): # items = keys + values
+#   print(key, value)
+
+# # Coding Excercise 28: Totaling Donations Excercise
+# donations = dict(sam=25.0, lena=88.99, chuck=13.0, linus=99.5, stan=150.0, lisa=50.25, harrison=10.0)
+# total_donations = 0
+# for value in donations.values():
+#     total_donations += value
+
+# total_donations = sum(donations.values()) # Better method, not covered yet
