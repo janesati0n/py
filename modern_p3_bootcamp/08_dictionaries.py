b91a481baf784a0c3f9be73e8ae9a3a9ba5eec61
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-04-08 12:32:16 +0200

    Update 08_dictionaries.py

--- modern_p3_bootcamp/08_dictionaries.py
+++ modern_p3_bootcamp/08_dictionaries.py
@@ -1,3 +1,11 @@
-# Creating Dictionaries
+# # Creating Dictionaries
+instructor = {
+  "name": "Colt",
+  "owns_dog": True,
+  "num_courses": 4,
+  "favorite_language": "Python",
+  "is_hilarious": False,
+  44: "my favorite number!"
+}
 
-# Coding Excercise 26
+# # Coding Excercise 26
