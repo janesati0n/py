aa8c72232e478d528795b8ac643a0a3dd8df2a25
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-04-08 12:41:19 +0200

    Update 08_dictionaries.py

--- modern_p3_bootcamp/08_dictionaries.py
+++ modern_p3_bootcamp/08_dictionaries.py
@@ -1,19 +1,24 @@
 # # Creating Dictionaries
-instructor = {
-  "name": "Colt",
-  "owns_dog": True,
-  "num_courses": 4,
-  "favorite_language": "Python",
-  "is_hilarious": False,
-  44: "my favorite number!"
-}
+# instructor = {
+#   "name": "Colt",
+#   "owns_dog": True,
+#   "num_courses": 4,
+#   "favorite_language": "Python",
+#   "is_hilarious": False,
+#   44: "my favorite number!"
+# }
 
-cat = dict(name ='kitty')
-cat = dict(name ='kitty', age=0.5)
+# cat = dict(name ='kitty')
+# cat = dict(name ='kitty', age=0.5)
 
 # # Coding Excercise 26
-user_info = {
-    'username': 'nbusr',
-    'password': 'nbusr123',
-    'date_created': 2024-4-8
-}
+# user_info = {
+#     'username': 'nbusr',
+#     'password': 'nbusr123',
+#     'date_created': 2024-4-8
+# }
+
+# # Accessing Data in Dictionaries
+# instructor['name'] # 'Colt'
+# instructor['thing'] # KeyError
+
