b32031eed48654a5b98171b75505befc65ea0aaa
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-04-08 15:03:14 +0200

    Update 08_dictionaries.py

--- modern_p3_bootcamp/08_dictionaries.py
+++ modern_p3_bootcamp/08_dictionaries.py
@@ -111,3 +111,9 @@
 # second['a'] = "AMAZING" # overwrites an existing key
 # second.update(first) # the update overrides our values
 # second # {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+
+# # Coding Exercise 31: Dictionary Methods Excercise
+# inventory = {'croissant': 19, 'bagel': 4, 'muffin': 8, 'cake': 1}
+# stock_list = inventory.copy() # Make a copy of inventory and save it to a variable called stock_list
+# stock_list['cookie'] = 18 # Add the value 18 to stock_list under the key "cookie"
+# stock_list.pop('cake') # Remove 'cake' from stock_list
