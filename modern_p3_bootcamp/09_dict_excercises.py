b94d18e8498fd475dc6ff47949bef708bc24a82f
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-04-09 14:15:22 +0200

    Update 09_dict_excercises.py

--- modern_p3_bootcamp/09_dict_excercises.py
+++ modern_p3_bootcamp/09_dict_excercises.py
@@ -1,1 +1,42 @@
-  # # Spotify Playlist Example
+# # Spotify Playlist Example
+# playlist = {
+#   'title': 'Patagonia Bus',
+#   'author': 'Colt Steele',
+#   'songs': [
+#     {'title': 'Turn it Off',
+#      'artist': ['Culture Abuse'],
+#      'duration': 3.37},
+#     {'title': 'Eating Hooks - Siriusmo Remix - Solomun Edit',
+#      'artist': ['Moderat', 'Siriusmo', 'Solomun'],
+#      'duration': 7.02},
+#      {'title': 'Nights Off',
+#      'artist': ['Siriusmo'],
+#      'duration': 4.08},
+#      {'title': 'Tilted - Paradis Remix',
+#      'artist': ['Christine and the Queens', 'Paradis'],
+#      'duration': 5.50},
+#      {'title': "Dont't Stop",
+#      'artist': ['Knightlife'],
+#      'duration': 6.13},
+#      {'title': 'Each Moment Like The First',
+#      'artist': ['James Holden', 'The Animal Spirits'],
+#      'duration': 4.57},
+#      {'title': 'Renata',
+#      'artist': ['James Holden'],
+#      'duration': 5.57},
+#      {'title': 'Sad Saturdays',
+#      'artist': ['JOBA'],
+#      'duration': 4.27},
+#      {'title': 'Give It To Me (Like You Mean It)',
+#      'artist': ['Cub Sport'],
+#      'duration': 2.50},
+#      {'title': 'Wasted Days',
+#      'artist': ['Cloud Nothings'],
+#      'duration': 8.54}
+#   ]
+# }
+
+# for song in playlist['songs']:
+#   print(song['title'])
+
+# # Dictionary Comprehension
