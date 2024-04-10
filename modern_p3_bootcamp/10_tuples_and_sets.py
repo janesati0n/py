64c27c471b0da8d5c7bd1f71ccd61f972b082c5e
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-04-10 17:21:49 +0200

    Update 10_tuples_and_sets.py

--- modern_p3_bootcamp/10_tuples_and_sets.py
+++ modern_p3_bootcamp/10_tuples_and_sets.py
@@ -27,5 +27,33 @@ months = ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'Augus
 # print(len(set(cities)))
 
 # # Set Methods and Set Math
+# s = set([1,2,3])
+# s.add(4) # {1,2,3,4}
+# set1 = {1,2,3,4,5,6}
+# s.remove(3) # {1,2,4,5,6}
+# another_s = s.copy() # {1,2,3}
+# s.clear() # set()
+
+# math_students = {'Matthew', 'Helen', 'Prashant', 'James', 'Aparna'}
+# biology_students = {'Jane', 'Matthew', 'Charlotte', 'Mesut', 'Oliver', 'James'}
+# math_students | biology_students # UNION (combine both sets)
+# {'Charlotte', 'Prashant', 'Jane', 'James', 'Oliver', 'Mesut', 'Helen', 'Aparna', 'Matthew'}
+
+# math_students & biology_students # SET INTERSECTIONS
+# {'James', 'Matthew'}
 
 # # Coding Excercise 36: Tuples and Sets Excercise
+# 1 - Create a variable called numbers which is a tuple with the the values 1, 2, 3 and 4 inside.
+# numbers = (1,2,3,4)
+# 2 - Create a variable called value which is a tuple with the the value 1 inside.
+# value = (1,)
+# 3 - Given the following variable:
+# values = [10,20,30]
+# Create a variable called static_values which is the result of the values variable converted to a tuple
+# static_values = tuple(values)
+# 4 - Given the following variable
+# stuff = [1,3,1,5,2,5,1,2,5]
+# Create a variable called unique_stuff which is a set of only the unique values in the stuff list
+# unique_stuff = set(stuff)
+
+# # Set Comprehension
