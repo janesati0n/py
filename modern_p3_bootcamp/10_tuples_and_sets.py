ec22139535437d64d67b2eda1cb26d66de6ee40b
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-04-10 16:42:23 +0200

    Update and rename 10_tuples_sets.py to 10_tuples_and_sets.py

---
+++ modern_p3_bootcamp/10_tuples_and_sets.py
@@ -0,0 +1,20 @@
+# # Tuple Looping and Methods
+months = ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December')
+
+# for month in months:
+#   print(month)
+
+# i = len(months) -1
+# while i >= 0:
+#   print(months[i])
+#   i -= 1
+
+# x = (1,2,3,3,3)
+# x.count(1) # 1
+# x.count(3) # 3
+
+# t = (1,2,3,3,3)
+# t.index(1) # 0
+# t.index(5) # ValueError
+# t.index(3) # 2 (first matching index)
+
