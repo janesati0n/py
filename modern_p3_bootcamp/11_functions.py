8e196976dc650eca05707a560c29b2b63b9aea95
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-04-15 11:56:35 +0200

    Update 11_functions.py

--- modern_p3_bootcamp/11_functions.py
+++ modern_p3_bootcamp/11_functions.py
@@ -1,2 +1,12 @@
 # # Defining Functions
+# def sing_happy_birthday():
+#   print('Happy Birthday To You')
+#   print('Happy Birthday To You')
+#   print('Happy Birthday Dear You')
+#   print('Happy Birthday To You')
+# sing_happy_birthday()
 
+# # Coding Excercise 37: Your first function
+# def make_noise():
+#     print('THE CROWD GOES WILD')
+# make_noise()
