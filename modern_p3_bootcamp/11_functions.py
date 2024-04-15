cee7aa7e75b84d8cdefc1064f59815b423ced92d
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-04-15 14:24:14 +0200

    Update 11_functions.py

--- modern_p3_bootcamp/11_functions.py
+++ modern_p3_bootcamp/11_functions.py
@@ -44,3 +44,28 @@
 #   return result
 
 # # Parameters
+# def square(num):
+#   return num * num
+# print(square(4))
+# print(square(8))
+
+# def sing_happy_birthday(name):
+#   print('Happy Birthday To You')
+#   print('Happy Birthday To You')
+#   print(f'Happy Birthday Dear {name}')
+#   print('Happy Birthday To You')
+# sing_happy_birthday('Rashida')
+# sing_happy_birthday('Nicolas')
+
+# # Parameters vs Arguments
+# def divide(num1, num2):
+#   return num1/num2
+# print(divide(2,5))
+# print(divide(5,2))
+
+# # Coding Excercise 40: Yell Function Excercise
+# def yell(string):
+#     return string.upper() + "!"     # variant A: string concatenation
+#     return "{}!".format(word.upper())   # variant B: format method
+#     return f"{word.upper()}!"       # variant C: f-string
+
