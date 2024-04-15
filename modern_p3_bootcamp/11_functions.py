36ebfd7cea4fb5341398968d827b7c85b5144358
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-04-15 12:21:17 +0200

    Update 11_functions.py

--- modern_p3_bootcamp/11_functions.py
+++ modern_p3_bootcamp/11_functions.py
@@ -10,3 +10,21 @@
 # def make_noise():
 #     print('THE CROWD GOES WILD')
 # make_noise()
+
+# # The Magical Return Keyword
+# def square_of_7():
+#   return 7**2
+# result = square_of_7()
+# print(result)
+
+# # Writing a coin_flip Function Using Random
+# from random import random
+
+# def flip_coin():
+#   if random() > 0.5:
+#     return "Heads"
+#   else:
+#     return "Tails"
+# print(flip_coin())
+
+# # Coding Excercise 38: Super Quick Return Excercise
