b858ecb4e557f590eebef7814ad7b1b711dfc585
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-04-16 17:01:46 +0200

    Update 12_functions_excercises.py

--- modern_p3_bootcamp/12_functions_excercises.py
+++ modern_p3_bootcamp/12_functions_excercises.py
@@ -4,3 +4,18 @@
 # product(2,2) # 4
 # product(2,-2) # -4
 
+# # Coding Excercise 44: Return Day
+# def return_day(x):
+#     days = {1:'Sunday', 2:'Monday', 3:'Tuesday', 4:'Wednesday', 5:'Thursday', 6:'Friday', 7:'Saturday'}
+#     return days.get(x)
+
+# return_day(1) # "Sunday"
+# return_day(2) # "Monday"
+# return_day(3) # "Tuesday"
+# return_day(4) # "Wednesday"
+# return_day(5) # "Thursday"
+# return_day(6) # "Friday"
+# return_day(7) # "Saturday"
+# return_day(41) # None
+
+
