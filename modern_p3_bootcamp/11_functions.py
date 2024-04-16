14f4e5d8683a6ec1f28aaf96696d80f7a30f1614
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-04-16 11:51:43 +0200

    Update 11_functions.py

--- modern_p3_bootcamp/11_functions.py
+++ modern_p3_bootcamp/11_functions.py
@@ -173,3 +173,9 @@
 #     count += 1
 #     return count
 #   return inner()
+
+# # Documenting Functions
+# def say_hello():
+#   """A simple function that returns the string hello"""
+#   return 'Hello!'
+# say_hello.__doc__ # 'A simple function that returns the string hello'
