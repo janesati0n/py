ae0d0006d27b52651972cf59a44205f8263ec502
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-04-16 17:01:58 +0200

    Update 12_functions_excercises.py

--- modern_p3_bootcamp/12_functions_excercises.py
+++ modern_p3_bootcamp/12_functions_excercises.py
@@ -10,12 +10,5 @@
 #     return days.get(x)
 
 # return_day(1) # "Sunday"
-# return_day(2) # "Monday"
-# return_day(3) # "Tuesday"
-# return_day(4) # "Wednesday"
-# return_day(5) # "Thursday"
-# return_day(6) # "Friday"
 # return_day(7) # "Saturday"
 # return_day(41) # None
-
-
