607427251af4d26ad55de76f30c96107103c3888
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-04-17 12:03:58 +0200

    Update 12_functions_excercises.py

--- modern_p3_bootcamp/12_functions_excercises.py
+++ modern_p3_bootcamp/12_functions_excercises.py
@@ -34,3 +34,8 @@
 # number_compare(2,4) # "Second is greater"
 
 # # Coding Excercise 47: Single Letter Count
+# def single_letter_count(string,letter):
+#   return string.lower().count(letter.lower())
+# single_letter_count("Hello World", "h") # 1
+# single_letter_count("Hello World", "z") # 0
+# single_letter_count("HelLo World", "l") # 3
