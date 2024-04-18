f129615e55ae5bdd01c30333451f09eb875f5804
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-04-18 15:00:31 +0200

    Update 12_functions_excercises.py

--- modern_p3_bootcamp/12_functions_excercises.py
+++ modern_p3_bootcamp/12_functions_excercises.py
@@ -103,3 +103,9 @@
 # intersection(['a','b','z'], ['x','y','z']) .  # ['z']
 
 # # Coding Excercise 56" Partition
+# def partition(lst, fn):
+#   return [[val for val in lst if fn(val)], [val for val in lst if not fn(val)]]
+
+# def isEven(num):
+#   return num % 2 == 0
+# partition([1,2,3,4], isEven) # [[2,4],[1,3]]
