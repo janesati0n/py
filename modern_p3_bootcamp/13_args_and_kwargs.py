ee6cbe37c46d639ed775308c292cf5b90e6ecb2a
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-04-19 14:59:44 +0200

    Update 13_args_and_kwargs.py

--- modern_p3_bootcamp/13_args_and_kwargs.py
+++ modern_p3_bootcamp/13_args_and_kwargs.py
@@ -1,1 +1,17 @@
+# # Introduction to *args
+# def sum_all_nums(*args):
+#   total = 0
+#   for num in args:
+#     total += num
+#   return total
 
+# print(sum_all_nums(4,6,9,4,10))
+# print(sum_all_nums(4,6))
+
+# def ensure_correct_info(*args):
+#   if "Colt" in args and "Steele" in args:
+#     return "Welcome back Colt!"
+#   return "Not sure who you are"
+
+# print(ensure_correct_info("hello", False, 78)) # Not sure who you are
+# print(ensure_correct_info(1, True, "Steele", "Colt")) # Welcome back Colt!
