52d70302129c9e929aaa312d6b5a3eb343151938
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-04-22 23:28:14 +0200

    Update 13_args_and_kwargs.py

--- modern_p3_bootcamp/13_args_and_kwargs.py
+++ modern_p3_bootcamp/13_args_and_kwargs.py
@@ -26,7 +26,17 @@
 # def fav_colors(**kwargs):
 #   for person, color in kwargs.items():  # key, value = person, color
 #     print(f"{person}'s favorite color is {color}")
-
 # fav_colors(colt='purple', ruby='red', ethel='teal')
+
+# def special_greeting(**kwargs):
+#   if "David" in kwargs and kwargs ["David"] == "special":
+#     return "You get a special greeting David!"
+#   elif "David" in kwargs:
+#     return f"{kwargs ['David']} David!"
+#   return "Not sure who this is..."
+
+# print(special_greeting(David='Hello')) # Hello David!
+# print(special_greeting(Bob='hello')) # Not sure who this is...
+# print(special_greeting(David='special')) # You get a special greeting David!
 
 # # Coding Excercise 58: Kwargs
