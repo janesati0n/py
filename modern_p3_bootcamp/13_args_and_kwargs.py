6924ce98693d8ad1d5a340d2c01ea0ea7b8a0a77
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-04-22 23:50:42 +0200

    Update 13_args_and_kwargs.py

--- modern_p3_bootcamp/13_args_and_kwargs.py
+++ modern_p3_bootcamp/13_args_and_kwargs.py
@@ -40,3 +40,15 @@
 # print(special_greeting(David='special')) # You get a special greeting David!
 
 # # Coding Excercise 58: Kwargs
+# def combine_words(word, **kwargs):
+#   if 'prefix' in kwargs:
+#     return kwargs['prefix'] + word
+#   elif 'suffix' in kwargs:
+#     return word + kwargs['suffix']
+#   return word
+
+# combine_words("child")  #'child'
+# combine_words("child", prefix="man")  #'manchild'
+# combine_words("child", suffix="ish")  #'childish'
+# combine_words("work", suffix="er")  #'worker'
+# combine_words("work", prefix="home")  #'homework'
