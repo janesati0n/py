5f966edaa768d3068ffe229b292fcdf5448b03e3
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-04-24 15:27:37 +0200

    Update 13_args_and_kwargs.py

--- modern_p3_bootcamp/13_args_and_kwargs.py
+++ modern_p3_bootcamp/13_args_and_kwargs.py
@@ -96,4 +96,19 @@
 # add_and_multiply_numbers(**data, cat="Blue")
 
 # # Coding Excercise 60: Calculate
-
+# def calculate(**kwargs):
+#   operation_lookup = {
+#     'add': kwargs.get('first', 0) + kwargs.get('second', 0),
+#     'subtract': kwargs.get('first', 0) - kwargs.get('second', 0),
+#     'divide': kwargs.get('first', 0) / kwargs.get('second', 0),
+#     'multiply': kwargs.get('first', 0) * kwargs.get('second', 0)
+#     }
+#   is_float = kwargs.get('make_float', False)
+#   operation_value = operation_lookup[kwargs.get('operation', '')]
+#   if is_float:
+#     final = f"{kwargs.get('message','The result is')} {float(operation_value)}"
+#   else:
+#     final = f"{kwargs.get('message','The result is')} {int(operation_value)}"
+#   return final
+# calculate(make_float=False, operation='add', message='You just added', first=2, second=4) # "You just added 6"
+# calculate(make_float=True, operation='divide', first=3.5, second=5) # "The result is 0.7"
