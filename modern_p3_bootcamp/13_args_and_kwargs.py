f90def25d92d4ac652bb6634b2069582b4261af9
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-04-24 14:31:45 +0200

    Update 13_args_and_kwargs.py

--- modern_p3_bootcamp/13_args_and_kwargs.py
+++ modern_p3_bootcamp/13_args_and_kwargs.py
@@ -80,3 +80,20 @@
 # nums = [90,1,35,67,89,20,3,1,2,3,4,5,6,9,34,46,57,68,79,12,23,34,55,1,90,54,34,76,8,23,34,45,56,67,78,12,23,34,45,56,67,768,23,4,5,6,7,8,9,12,34,14,15,16,17,11,7,11,8,4,6,2,5,8,7,10,12,13,14,15,7,8,7,7,345,23,34,45,56,67,1,7,3,6,7,2,3,4,5,6,7,8,9,8,7,6,5,4,2,1,2,3,4,5,6,7,8,9,0,9,8,7,8,7,6,5,4,3,2,1,7]
 # result1 = count_sevens(1,4,7)
 # result2 = count_sevens(*nums)
+
+# # Dictionary Unpacking
+# def display_names(first, second):
+#   print(f"{first} says hello to {second}")
+# names = {"first": "Colt", "second": "Rusty"}
+# display_names(first="Charlie", second="Sue")
+# display_names(**names)
+
+# def add_and_multiply_numbers(a,b,c,**kwargs):
+#   print(a + b * c)
+#   print("OTHER STUFF...")
+#   print(kwargs)
+# data = dict(a=1,b=2,c=3,d=55,name="Tony")
+# add_and_multiply_numbers(**data, cat="Blue")
+
+# # Coding Excercise 60: Calculate
+
