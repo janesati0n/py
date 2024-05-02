3f70dd8af01349a7ac83a77dc7c5c57e8e8fc7c7
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-05-02 09:39:29 +0200

    Update 14_lambdas.py

--- modern_p3_bootcamp/14_lambdas.py
+++ modern_p3_bootcamp/14_lambdas.py
@@ -6,3 +6,23 @@
 # # Coding Excercise 61: Lambda
 # cube = lambda num: num ** 3
 
+# # Map
+# nums = [2,4,6,8,10]
+# doubles = list(map(lambda x: x*2,nums))
+# doubles # [4, 8, 12, 16, 20]
+
+# people = ['Darcy', 'Christina', 'Dana', 'Annabel']
+# peeps = list(map(lambda name: name.upper(), people))
+# peeps # ['DARCY', 'CHRISTINA', 'DANA', 'ANNABEL']
+
+# names = [
+#   {'first':'Rusty', 'last':'Steele'},
+#   {'first':'Colt', 'last':'Steele'},
+#   {'first':'Blue', 'last':'Steele'}
+# ]
+# first_names = list(map(lambda x: x['first'], names))
+# first_names # ['Rusty', 'Colt', 'Blue']
+
+# def double(x): return x*2
+# doubles = map(double, nums)
+# list(doubles) # [4, 8, 12, 16, 20]
