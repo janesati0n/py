2c76534ad2a5a72e45235ce5c43902841d3282d1
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-06-17 12:17:36 +0200

    Update 14_lambdas.py

--- modern_p3_bootcamp/14_lambdas.py
+++ modern_p3_bootcamp/14_lambdas.py
@@ -240,3 +240,10 @@ students = ['dan', 'ang', 'kate']
 # interleave('hi', 'ha') # hhia
 # interleave('aaa', 'zzz') # azazaz
 # interleave('lzr', 'iad') # lizard
+
+# # Coding Excercise 70: triple_and_filter
+# def triple_and_filter(lst):
+#   return list(filter(lambda x: x % 4 == 0, map(lambda x: x*3, lst)))
+
+# triple_and_filter([1,2,3,4]) # [12]
+# triple_and_filter([6,8,10,12]) # [24,36]
