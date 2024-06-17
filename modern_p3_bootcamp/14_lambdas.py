5927f889c85e2b6766aa2816a5ce2ec33c15228a
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-06-17 12:23:31 +0200

    Update 14_lambdas.py

--- modern_p3_bootcamp/14_lambdas.py
+++ modern_p3_bootcamp/14_lambdas.py
@@ -247,3 +247,10 @@ students = ['dan', 'ang', 'kate']
 
 # triple_and_filter([1,2,3,4]) # [12]
 # triple_and_filter([6,8,10,12]) # [24,36]
+
+# # Coding Excercise 71: extract_full_name
+# def extract_full_name(l):
+#   return list(map(lambda val: f"{val['first']} {val['last']}", l))
+
+# names = [{'first': 'Elie', 'last': 'Schoppik'}, {'first': 'Colt', 'last': 'Steele'}]
+# extract_full_name(names) # ['Elie Schoppik', 'Colt Steele']
