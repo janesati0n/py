d6725c10033eba4e587432b01e65494e6185f325
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-06-19 00:08:59 +0200

    Update 15_debugging.py

--- modern_p3_bootcamp/15_debugging.py
+++ modern_p3_bootcamp/15_debugging.py
@@ -1,8 +1,24 @@
 # # Common Types of Errors
+
 # SyntaxError # typos # incorrect syntax
+  #def name(( # SyntaxError
+
 # NameError # var not defined
+  # test # NameError
+
 # TypeError # operation or function on wrong type
+  # len(5) # TypeError
+
 # IndexError # invalid index (f.e. out of range of list)
+  # list = ['hello']
+  # list[2] # IndexError
+
 # ValueError # argument has right type but inappropriate value
+  # int('foo') # ValueError
+
 # KeyError # dictionary does not have a specific key
+  # d = {}
+  # d ['foo'] # KeyError
+
 # AttributeError # var does not have an attribute
+  # 'awesome'.foo # AttributeError
