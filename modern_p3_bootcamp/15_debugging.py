f8e9d3bc8d5f418b883bd9a2de83e6546d0a2546
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-06-19 00:02:02 +0200

    Update 15_debugging.py

--- modern_p3_bootcamp/15_debugging.py
+++ modern_p3_bootcamp/15_debugging.py
@@ -1,1 +1,8 @@
 # # Common Types of Errors
+# SyntaxError # typos # incorrect syntax
+# NameError # var not defined
+# TypeError # operation or function on wrong type
+# IndexError # invalid index (f.e. out of range of list)
+# ValueError # argument has right type but inappropriate value
+# KeyError # dictionary does not have a specific key
+# AttributeError # var does not have an attribute
