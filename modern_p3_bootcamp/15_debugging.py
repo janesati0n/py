98852794cf390cc614b52784b78c485d2edfe9b7
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-06-27 11:59:40 +0200

    Update 15_debugging.py

--- modern_p3_bootcamp/15_debugging.py
+++ modern_p3_bootcamp/15_debugging.py
@@ -118,3 +118,16 @@
 #   return a + b + c + d
 # add_numbers(1,2,3,4)
 # # to print value of c instead of quitting - use "p c"
+
+# # Coding Exercise 72: Debugging and Error Handling
+# def divide(num1, num2):
+#   try:
+#     total = num1 / num2
+#   except ZeroDivisionError:
+#     return "Please do not divide by zero"
+#   except TypeError:
+#     return "Please provide two integers or floats"
+#   return total
+# divide(4,2) # 2
+# divide([], "1") # "Please provide two integers or floats"
+# divide(1,0) # "Please do not divide by zero"
