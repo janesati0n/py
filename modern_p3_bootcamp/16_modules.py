26c39d38c10c1e1dac61a3c47b03c661569067bb
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-07-01 10:24:45 +0200

    Update 16_modules.py

--- modern_p3_bootcamp/16_modules.py
+++ modern_p3_bootcamp/16_modules.py
@@ -1,1 +1,15 @@
+# # Working with Built-in Modules
+# import random as rand
+# print(rand.choice(["apple", "banana", "cherry", "durian"]))
+# rand.shuffle(["apple", "banana", "cherry", "durian"])
+
+# from random import choice, randint
+# print(randint(1,100))
+# print(shuffle(["apple", "banana", "cherry", "durian"])) # NameError
+
+# from random import choice as gimme_one, shuffle as mix_up_fruits
+# gimme_one(["apple", "banana", "cherry", "durian"])
+# mix_up_fruits(["apple", "banana", "cherry", "durian"])
+
+# # Coding Excercise 73: Built in Modules
 
